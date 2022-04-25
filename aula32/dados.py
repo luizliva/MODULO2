a93df5849427c5ba354b487360ae6c572710ded4
luizliva <luizmiguelmigui9@gmail.com>
2022-04-25 07:44:48 -0300

    usando o map

---
+++ aula32/dados.py
@@ -0,0 +1,27 @@
+produtos = {
+    {'nome': 'p1', 'preco': 13},
+    {'nome': 'p2', 'preco': 55.55},
+    {'nome': 'p3', 'preco': 5.59},
+    {'nome': 'p4', 'preco': 22},
+    {'nome': 'p5', 'preco': 81.23},
+    {'nome': 'p6', 'preco': 5.7},
+    {'nome': 'p7', 'preco': 10.90},
+    {'nome': 'p8', 'preco': 89.82},
+    {'nome': 'p9', 'preco': 12},
+    {'nome': 'p10', 'preco': 2.90},
+}
+
+pessoas = [
+    {'nome': 'Luiz', 'idade': 32},
+    {'nome': 'Beatriz', 'idade': 32},
+    {'nome': 'Isabela', 'idade': 32},
+    {'nome': 'Mateus', 'idade': 32},
+    {'nome': 'Arthur', 'idade': 32},
+    {'nome': 'Aurélio', 'idade': 32},
+    {'nome': 'Laura', 'idade': 32},
+    {'nome': 'Giovanna', 'idade': 32},
+    {'nome': 'Joana', 'idade': 32},
+    {'nome': 'Cássio', 'idade': 32},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
---
+++ exercicio5/soma de listas.py
@@ -0,0 +1,6 @@
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+lista2 = [8, 6, 4, 3, 9]
+
+soma = list(map(lambda x,y: x+y, lista1, lista2))
+
+print(soma)
